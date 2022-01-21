8ab5316b43ca8c2648d083f2f0a2307acf642f0f
Mohamed Dhik <92504802+mohamedhik@users.noreply.github.com>
2022-01-21 23:12:06 +0100

    Create main.go

---
+++ Base64-Enconding/main.go
@@ -0,0 +1,14 @@
+func main(){
+message := "hello,go (*w3hu%#"
+    demoBase64(message)
+}
+func demoBase64(message string) { fmt.Println("--------Demo encoding base64--------")
+    fmt.Println("plaintext:")
+    fmt.Println(message)
+    encoding := base64.StdEncoding.EncodeToString([]byte(message))
+    fmt.Println("base64 msg:")
+    fmt.Println(encoding)
+    decoding, _ := base64.StdEncoding.DecodeString(encoding)
+    fmt.Println("decoding base64 msg:")
+    fmt.Println(string(decoding))
+}
